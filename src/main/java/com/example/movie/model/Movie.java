33896aa5ade4fcd9597b40ed341d1a33b8140bab
tejasribarla <tejasribarla188@gmail.com>
2023-03-27 10:22:40 +0000

    solution

---
+++ src/main/java/com/example/movie/model/Movie.java
@@ -0,0 +1,55 @@
+/*
+ * You can use the following import statements
+ * 
+ * import javax.persistence.*;
+ * 
+ */
+
+// Write your code here
+
+package com.example.movie.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="movielist")
+
+public class Movie{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="movieid")
+    private int movieId;
+
+    @Column(name="moviename")
+    private String movieName;
+
+    @Column(name="leadactor")
+    private String leadActor;
+
+    public Movie(){}
+
+    public Movie(int movieId, String movieName, String leadActor) {
+        this.movieId=movieId;
+        this.movieName=movieName;
+        this.leadActor=leadActor;
+    } 
+
+    public int getMovieId() {
+        return this.movieId;
+    }
+    public void setMovieId(int movieId) {
+        this.movieId=movieId;
+    }
+    public String getMovieName(){
+        return this.movieName;
+    }
+    public void setMovieName(String movieName){
+        this.movieName=movieName;
+    }
+    public String getLeadActor(){
+        return this.leadActor;
+    }
+    public void setLeadActor(String leadActor) {
+        this.leadActor=leadActor;
+    }
+}
---
+++ src/main/java/com/example/movie/repository/MovieJpaRepository.java
@@ -0,0 +1,20 @@
+/*
+ * You can use the following import statements
+ * 
+ * import org.springframework.data.jpa.repository.JpaRepository;
+ * import org.springframework.stereotype.Repository;
+ * 
+ */
+
+
+package com.example.movie.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.movie.model.Movie;
+
+
+@Repository
+public interface MovieJpaRepository extends JpaRepository <Movie,Integer> {
+
+}
